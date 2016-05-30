0777319e14d782be2f95bd69585ae032fff0eba5
vireasss <vireas@mail.ru>
2016-05-30 16:46:16 +0400

    ready search

--- test/vireasss/arrays/solution.rb
+++ test/vireasss/arrays/solution.rb
@@ -7,11 +7,22 @@ module Vireasss
       end
 
       def search(array, query)
-        0
-      end
+          len = array.size
+          i = 0
+          get_index = len / 2
+          while array[get_index] != query and i < len do
+            if query > array[get_index]
+              i = get_index + 1
+            else
+              len = get_index - 1
+            end
+            get_index = (i + len) / 2
+          end
+          if array[get_index] != query
+            get_index = -1
+          end
+        return get_index
+       end
     end
   end
 end
-
-array = [ 3, 2, -8, 4, 1, -6, 7, 8, -99 ]
-puts new_array = Vireasss::Arrays.replace(array)
