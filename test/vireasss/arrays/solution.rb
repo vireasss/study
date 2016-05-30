c010ff9f2d469ec087ad70dcbabc24a6f5f9a63c
vireasss <vireas@mail.ru>
2016-05-30 15:40:30 +0400

    ready test_replace

--- test/vireasss/arrays/solution.rb
+++ test/vireasss/arrays/solution.rb
@@ -2,7 +2,8 @@ module Vireasss
   module Arrays
     class << self
       def replace(array)
-        array
+        new_array = array.map { |e| (e < 0 ? e = array.min : e ) }
+
       end
 
       def search(_array, _query)
@@ -11,3 +12,6 @@ module Vireasss
     end
   end
 end
+
+array = [ 3, 2, -8, 4, 1, -6, 7, 8, -99 ]
+puts new_array = Vireasss::Arrays.replace(array)
