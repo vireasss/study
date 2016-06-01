16977d2f8ddc5466d009db4df99b7bc43670208f
vireasss <vireas@mail.ru>
2016-06-01 11:21:47 -0700

    fix remarks correction #2

--- test/vireasss/fp/solution.rb
+++ test/vireasss/fp/solution.rb
@@ -4,14 +4,14 @@ module Vireasss
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array, _access_level)
+      def rating(array, access_level)
 
-        _array = _array.map { |_array| _array if _array["access_level"].to_i >= _access_level.to_i and _array["rating_kinopoisk"].to_f > 0}.compact
+        array = _array.map { |array| array if array['access_level'].to_i >= access_level.to_i && array['rating_kinopoisk'].to_f > 0 }.compact
         sum = 0
-        for film in _array
-          sum = sum + film["rating_kinopoisk"].to_f
+        for film in array
+          sum += film['rating_kinopoisk'].to_f
         end
-        sum / _array.size
+        sum / array.size
       end
 
       def countries_count(_films, _year)
