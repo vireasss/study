8a3830ebf790499e781b29033c4e73434e5ee98a
vireasss <vireas@mail.ru>
2016-06-01 16:18:19 +0400

    dont know

--- test/vireasss/arrays/solution.rb
+++ test/vireasss/arrays/solution.rb
@@ -6,9 +6,30 @@ module Vireasss
 
       end
 
+<<<<<<< e9f2d5a3e725d9384bee21b714cfddc6964f4cee
       def search(_array, _query)
         0
       end
+=======
+      def search(array, query)
+        len = array.size
+        array = array.sort
+        i = 0
+        get_index = len / 2
+        while array[get_index] != query and i < len do
+          if query > array[get_index]
+            i = get_index + 1
+          else
+            len = get_index - 1
+          end
+          get_index = (i + len) / 2
+        end
+        if array[get_index] != query
+          get_index = -1
+        end
+        return get_index
+       end
+>>>>>>> added sort array
     end
   end
 end
