b038a38a4d69fca5b33422981d7dd6011a188d74
vireasss <vireas@mail.ru>
2016-06-01 17:57:53 +0400

    ready to 90% - rating, have little errors

--- test/vireasss/fp/solution.rb
+++ test/vireasss/fp/solution.rb
@@ -5,11 +5,21 @@ module Vireasss
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array, _access_level)
-        0
+
+        _array = _array.map { |_array| _array if _array["access_level"].to_i >= _access_level.to_i and _array["rating_kinopoisk"].to_f > 0}.compact
+        sum = 0
+        for film in _array
+          sum = sum + film["rating_kinopoisk"].to_f
+        end
+        puts '++++++++++++++++++++++'
+        puts sum.to_f
+        puts _array.size
+        puts '++++++++++++++++++++++'
+        result = sum / _array.size
       end
 
       def countries_count(_films, _year)
-        0
+        puts 1
       end
     end
   end
