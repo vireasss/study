2740e4541998e04b862ddd5109474261cd11b279
vireasss <vireas@mail.ru>
2016-06-01 11:13:21 -0700

    fix remarks correction

--- test/vireasss/arrays/solution.rb
+++ test/vireasss/arrays/solution.rb
@@ -4,26 +4,25 @@ module Vireasss
       def replace(array)
         min_element = array.min
         array.map { |e| e < 0 ? min_element : e }
-
       end
 
       def search(array, query)
-          len = array.size
-          i = 0
-          get_index = len / 2
-          while array[get_index] != query and i < len do
-            if query > array[get_index]
-              i = get_index + 1
-            else
-              len = get_index - 1
-            end
-            get_index = (i + len) / 2
+        len = array.size
+        i = 0
+        get_index = len / 2
+        while array[get_index] != query && i < len
+          if query > array[get_index]
+            i = get_index + 1
+          else
+            len = get_index - 1
           end
-          if array[get_index] != query
-            get_index = -1
-          end
-          get_index
-       end
+          get_index = (i + len) / 2
+        end
+        if array[get_index] != query
+          get_index = -1
+        end
+        get_index
+      end
     end
   end
 end
